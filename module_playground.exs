730a195c8db0f2499371cf06eae4ca856d0ce95c
Juyoung Choi <juyoung.choi@lego.com>
2023-06-30 21:57:42 +0900

    [2023-06-30] use a function from another module

--- module_playground.exs
+++ module_playground.exs
@@ -1,5 +1,18 @@
 defmodule ModulePlayground do
+    import IO, only: [puts: 1] # import only IO.puts/1
+    import Kernel, except: [inspect: 1] # import all Kernel functions except Kernel.inspect/1 in case where there is local function with same name.
+
     def say_here do
-        IO.puts "I am here"
+        inspect "I am here"
     end
+
+    def inspect(param1) do
+        puts "Starting Output"
+        puts param1
+        puts "Ending Output"
+    end
+
+    def print_sum do
+        ModulePlayground.Misc.Util.Math.add(1, 2)
+    end    
 end
