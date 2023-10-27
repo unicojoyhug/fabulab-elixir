9e38dab6b871ad4178b5d1eb70fb5d9504532877
Juyoung Choi <juyoung.choi@lego.com>
2023-10-27 14:08:20 +0200

    [2023-10-27] add test

--- hello_world/test/hellow_world/file_reader_test.exs
+++ hello_world/test/hellow_world/file_reader_test.exs
@@ -1,11 +1,19 @@
 defmodule HellowWorld.FileReaderTest do
   use ExUnit.Case
+  import IO, only: [puts: 1]
   import HelloWorld.FileReader
 
   test "Passing a file should return a string" do
     str = get_strings_to_tweet(Path.join("#{:code.priv_dir(:hello_world)}", "sample.txt"))
+    puts("random string found: #{str}")
 
     assert str != nil
   end
 
+  test "Will not return a string longer than 140 characters" do
+    str =
+      get_strings_to_tweet(Path.join("#{:code.priv_dir(:hello_world)}", "/test/too_long.txt"))
+
+    assert str == "short line"
+  end
 end
